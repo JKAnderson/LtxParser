bdd6c8384c98c07c30dff109a2c7e3f021fdc908
Joseph Anderson <josepha93@gmail.com>
2017-06-01 19:40:05 -0400

    Add assembly description

--- LtxParser/Properties/AssemblyInfo.cs
+++ LtxParser/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LtxParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# library for loading .ltx files from the STALKER series")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LtxParser")]
